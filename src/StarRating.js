34472d655c9d025b4b827a6c3cce6c01824c05f6
Oyefolu Toyyib Adeyemi <oyefolutoyyib16@gmail.com>
2024-07-24 14:48:38 +0100

    adding watched movie

--- src/StarRating.js
+++ src/StarRating.js
@@ -35,7 +35,7 @@ export default function StarRating({
 
   function handleRating(rating) {
     setRating(rating);
-    // onSetRating(rating);
+    onSetRating(rating);
   }
 
   const textStyle = {
