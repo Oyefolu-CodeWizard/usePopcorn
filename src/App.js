d2f31da68daa22ed4c8ca6b582189ed4ae8e1065
Oyefolu Toyyib Adeyemi <oyefolutoyyib16@gmail.com>
2024-07-29 22:36:51 +0100

    fixed bug

--- src/App.js
+++ src/App.js
@@ -291,7 +291,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
         try {
           setIsLoading(true);
           const res = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
 
           if (!res.ok)
